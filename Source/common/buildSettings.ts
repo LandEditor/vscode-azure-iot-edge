299b8b4aebc6229c190fd647987e53fdf0b67fd1
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-14 22:41:37 +0200

    squash!

--- Source/common/buildSettings.ts
+++ Source/common/buildSettings.ts
@@ -1,11 +1,11 @@
 export class BuildSettings {
-	public readonly options?: string[];
-	public readonly contextPath: string;
-	public readonly dockerFile: string;
+    public readonly options?: string[];
+    public readonly contextPath: string;
+    public readonly dockerFile: string;
 
-	constructor(dockerFile: string, contextPath: string, options?: string[]) {
-		this.dockerFile = dockerFile;
-		this.contextPath = contextPath;
-		this.options = options;
-	}
+    constructor(dockerFile: string, contextPath: string, options?: string[]) {
+        this.dockerFile = dockerFile;
+        this.contextPath = contextPath;
+        this.options = options;
+    }
 }
